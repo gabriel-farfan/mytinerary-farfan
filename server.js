9fb8289a46239a69d723e6e8074fc76014f208b8
Gabriel Farfán <gmfarfan85@gmail.com>
2022-03-30 15:40:36 -0300

    build

--- server.js
+++ server.js
@@ -9,22 +9,22 @@ const Router = require ('./routes/routes')
 const passport = require("passport")
 const app = express();
 const path = require('path')
-const PORT = process.env.PORT || 4000;
-const HOST = process.env.HOST || '0.0.0.0'
+// const PORT = process.env.PORT || 4000;
+// const HOST = process.env.HOST || '0.0.0.0'
 
 // Middlewares
 
 app.use(cors())
 app.use(express.json()) //para que me de respuestas en formato JSON
-app.use('/api', Router)
 app.use(passport.initialize())
+app.use('/api', Router)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname + "/client/build/index.html"));
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
     });
 }
 
 
-app.listen(PORT, HOST, () => console.log("Server ready on PORT" + PORT));
+app.listen( process.env.PORT || 4000, process.env.HOST || '0.0.0.0', () => console.log(`Server ready on PORT ${process.env.PORT || 4000} `));
